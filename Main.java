2e79d9b471782ecce48a4287ff0da35fcd4f4b5a
Pooja Lodhi <lodhipooja952@gmail.com>
2026-08-06 12:12:34 +0530

    Add Main class with ArrayList example

---
+++ Main.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Mango");
+
+        System.out.println(list);
+    }
+}
